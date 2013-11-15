d1c3764d81d875cc9a440b56c6636c8e8a02cabc
Dominik Grünert <dominik.gruenert@gmail.com>
2013-11-15 16:33:05 +0100

    nur zum testen

--- ElectronicHealthRecordServices/src/ch/bfh/www/ehrservices/EhrServicesSkeleton.java
+++ ElectronicHealthRecordServices/src/ch/bfh/www/ehrservices/EhrServicesSkeleton.java
@@ -1,49 +1,43 @@
-
 /**
  * EhrServicesSkeleton.java
  *
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
  */
-    package ch.bfh.www.ehrservices;
-    /**
-     *  EhrServicesSkeleton java skeleton for the axisService
-     */
-    public class EhrServicesSkeleton{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getDocumentById 
-             * @return getDocumentByIdResponse 
-         */
-        
-                 public ch.bfh.www.ehrservices.GetDocumentByIdResponse getDocumentById
-                  (
-                  ch.bfh.www.ehrservices.GetDocumentById getDocumentById
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getDocumentById");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getLogin 
-             * @return getLoginResponse 
-         */
-        
-                 public ch.bfh.www.ehrservices.GetLoginResponse getLogin
-                  (
-                  ch.bfh.www.ehrservices.GetLogin getLogin
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getLogin");
-        }
-     
-    }
-    
+package ch.bfh.www.ehrservices;
+
+/**
+ * EhrServicesSkeleton java skeleton for the axisService
+ */
+public class EhrServicesSkeleton {
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param getDocumentById
+	 * @return getDocumentByIdResponse
+	 */
+
+	public ch.bfh.www.ehrservices.GetDocumentByIdResponse getDocumentById(
+			ch.bfh.www.ehrservices.GetDocumentById getDocumentById) {
+		// testennnenen
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#getDocumentById");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param getLogin
+	 * @return getLoginResponse
+	 */
+
+	public ch.bfh.www.ehrservices.GetLoginResponse getLogin(
+			ch.bfh.www.ehrservices.GetLogin getLogin) {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#getLogin");
+	}
+
+}
